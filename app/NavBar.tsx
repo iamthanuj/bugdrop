b6a31e910f0c0429573c26617e654138951bb9b7
Thanuj <thanujdevil@gmail.com>
2024-09-21 17:03:22 +0530

    Navbar active link styles

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,16 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames'
 import logo from "@/public/svg-formatter-beautifier-_2_.png"
 import Image from 'next/image'
 
 
 const NavBar = () => {
 
+    const pathName = usePathname();
 
     const links = [
         {label: "Dashboard", href:'/'},
@@ -16,7 +21,13 @@ const NavBar = () => {
     <nav className='flex justify-between h-20 bg-maincolor items-center px-5 '>
         <Link href="/"><Image src={logo} alt='logo'width={150}/></Link>
         <ul className='flex gap-5'>
-            {links.map((link)=> <li key={link.href}><Link className='hover:text-thirdcolor transition-colors' href={link.href} >{link.label}</Link></li> )}
+            {links.map((link)=> <li key={link.href}><Link className={
+              classNames({
+                'text-secondcolor' : link.href === pathName,
+                'text-white' : link.href !== pathName,
+                'hover:text-thirdcolor transition-colors' : true
+              })
+            } href={link.href} >{link.label}</Link></li> )}
             
         </ul>
     </nav>
