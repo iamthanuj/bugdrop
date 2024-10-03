599a6edca8f9e9e837ae57c71b9fcd519fe5b9cf
Thanuj <thanujdevil@gmail.com>
2024-10-04 02:16:15 +0530

    view bugs

--- app/bugs/page.tsx
+++ app/bugs/page.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
+import React from "react";
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import prisma from "@/prisma/client";
 
-const BugsPage = () => {
+const BugsPage = async () => {
+  const bugs = await prisma.bug.findMany();
+
   return (
     <div>
-      <Button><Link href="/bugs/new">New Bug</Link></Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/bugs/new">New Bug</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {bugs.map((bug) => (
+            <Table.Row key={bug.id}>
+              <Table.Cell>{bug.title}</Table.Cell>
+              <Table.Cell>{bug.status}</Table.Cell>
+              <Table.Cell>{bug.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
-  )
-}
+  );
+};
 
-export default BugsPage
+export default BugsPage;
