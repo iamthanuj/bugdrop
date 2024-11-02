865080c6c1739d73224b696d63b51c83bf069797
Thanuj <thanujdevil@gmail.com>
2024-11-03 02:06:43 +0530

    Pop up confirmation

--- app/bugs/[id]/DeleteBugButton.tsx
+++ app/bugs/[id]/DeleteBugButton.tsx
@@ -1,11 +1,30 @@
-import { Button } from '@radix-ui/themes'
-import React from 'react'
-import {TrashIcon} from "@radix-ui/react-icons"
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import React from "react";
+import { TrashIcon } from "@radix-ui/react-icons";
 
-const DeleteBugButton = ({bugId}:{bugId:number}) => {
+const DeleteBugButton = ({ bugId }: { bugId: number }) => {
   return (
-    <Button color='red'><TrashIcon/>Delete</Button>
-  )
-}
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>Are you sure you want to delete this bug? This action cannot be undone!</AlertDialog.Description>
+        <Flex className="mt-4" gap={"10px"}>
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">Close</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Confirm</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
 
-export default DeleteBugButton
+export default DeleteBugButton;
