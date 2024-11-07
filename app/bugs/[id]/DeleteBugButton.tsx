1a0c593e47da0b173b5ddc0d13ab18aaf5b56d8c
Thanuj <thanujdevil@gmail.com>
2024-11-08 01:34:05 +0530

    error handler added

--- app/bugs/[id]/DeleteBugButton.tsx
+++ app/bugs/[id]/DeleteBugButton.tsx
@@ -1,40 +1,73 @@
-"use client"
+"use client";
 
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
-import React from "react";
+import React, { useState } from "react";
 import { TrashIcon } from "@radix-ui/react-icons";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
 const DeleteBugButton = ({ bugId }: { bugId: number }) => {
-
   const router = useRouter();
 
+  const [error, setError] = useState(false);
+
+  const deleteBug = async () => {
+    try {
+      await axios.delete("/api/bugs/" + bugId);
+      router.push("/bugs");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">
-          <TrashIcon />
-          Delete
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>Are you sure you want to delete this bug? This action cannot be undone!</AlertDialog.Description>
-        <Flex className="mt-4" gap={"10px"}>
-          <AlertDialog.Cancel>
-            <Button color="gray" variant="soft">Close</Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button color="red" onClick={async ()=>{
-              await axios.delete("/api/bugs/"+bugId);
-              router.push('/bugs');
-              router.refresh();
-            }}>Confirm</Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">
+            <TrashIcon />
+            Delete
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this bug? This action cannot be
+            undone!
+          </AlertDialog.Description>
+          <Flex className="mt-4" gap={"10px"}>
+            <AlertDialog.Cancel>
+              <Button color="gray" variant="soft">
+                Close
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={deleteBug}>
+                Confirm
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This bug could not be deleted
+          </AlertDialog.Description>
+          <Button
+            color="gray"
+            variant="soft"
+            onClick={() => {
+              setError(false);
+            }}
+          >
+            Ok
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
