5b2254fde49ff47aa3bd775fd643ed4771042f9c
Thanuj <thanujdevil@gmail.com>
2024-11-20 02:42:26 +0530

    nav refractor

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -10,63 +10,88 @@ import { usePathname } from "next/navigation";
 import { ExitIcon, EnterIcon } from "@radix-ui/react-icons";
 
 const NavBar = () => {
-  const pathName = usePathname();
-  const { status, data } = useSession();
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Bugs", href: "/bugs/list" },
-  ];
-
   return (
     <nav className="flex justify-between h-20 bg-maincolor items-center px-5 ">
       <Link href="/">
         <Image src={logo} alt="logo" width={150} />
       </Link>
       <div className="flex gap-10 items-center">
-        <ul className="flex gap-5">
-          {links.map((link) => (
-            <li key={link.href}>
-              <Link
-                className={classNames({
-                  "text-secondcolor": link.href === pathName,
-                  "text-white": link.href !== pathName,
-                  "hover:text-thirdcolor transition-colors": true,
-                })}
-                href={link.href}
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <Box>
-          {status === "authenticated" && (
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger>
-                <Avatar src={data.user!.image!} fallback="?" radius="full" referrerPolicy="no-referrer"/>
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Label>{data.user!.name!}</DropdownMenu.Label>
-                <DropdownMenu.Label>{data.user!.email!}</DropdownMenu.Label>
-                <DropdownMenu.Item>
-                  <Link href="/api/auth/signout/">Log Out</Link>
-                </DropdownMenu.Item>
-              </DropdownMenu.Content>
-            </DropdownMenu.Root>
-          )}
-          {status === "unauthenticated" && (
-            <Link
-              className="text-white bg-gray-500 p-2 rounded-sm flex  items-center gap-3"
-              href="/api/auth/signin"
-            >
-              <EnterIcon />
-              Log In
-            </Link>
-          )}
-        </Box>
+        <NavLink/>
+        <AuthStatus />
       </div>
     </nav>
+  );
+};
+
+const NavLink = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Bugs", href: "/bugs/list" },
+  ];
+
+  const pathName = usePathname();
+
+  return (
+    <ul className="flex gap-5">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "text-secondcolor": link.href === pathName,
+              "text-white": link.href !== pathName,
+              "hover:text-thirdcolor transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data } = useSession();
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    return (
+      <Link
+        className="text-white bg-gray-500 p-2 rounded-sm flex  items-center gap-3"
+        href="/api/auth/signin"
+      >
+        <EnterIcon />
+        Log In
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={data!.user!.image!}
+            fallback="?"
+            radius="full"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>{data!.user!.name!}</DropdownMenu.Label>
+          <DropdownMenu.Label>{data!.user!.email!}</DropdownMenu.Label>
+          <DropdownMenu.Item className="flex justify-center">
+            <Link
+              className="flex justify-center items-center gap-2"
+              href="/api/auth/signout/"
+            >
+              <ExitIcon /> Log Out
+            </Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
